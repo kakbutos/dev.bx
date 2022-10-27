a2da2300ef26c51aeee9a42d9a292c9bc138ec8e
AsadullinArtur <lon.don.91@bk.ru>
2022-10-27 17:01:43 +0200

    todo list на php консольное приложение

---
+++ todo.php
@@ -0,0 +1,48 @@
+<?php
+
+// php todo.php list
+// php todo.php list 2022-10-12
+// php todo.php list yesterday
+// php todo.php add "Wake up"
+// php todo.php add "Drink coffee"
+// php todo.php done 1 2
+// php todo.php undone 1 2
+// php todo.php remove 2 (rm)
+// php todo.php report
+
+require_once __DIR__ . '/boot.php';
+
+function main(array $arguments)
+{
+	array_shift($arguments);
+
+	$command = array_shift($arguments);
+
+	switch ($command)
+	{
+		case 'list':
+			listCommand($arguments);
+			break;
+		case 'add':
+			addCommand($arguments);
+			break;
+		case 'done':
+			doneCommand($arguments);
+			break;
+		case 'undone':
+			undoneCommand($arguments);
+			break;
+		case 'remove':
+		case 'rm':
+			removeCommand($arguments);
+			break;
+
+		default:
+			echo 'Unknown command';
+			exit(1);
+	}
+
+	exit(0);
+}
+
+main($argv);
