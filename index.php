af0a6eb38f9f49edf87e189932cd4afbf151d544
AsadullinArtur <lon.don.91@bk.ru>
2022-11-07 17:09:52 +0200

    верстка

---
+++ index.php
@@ -0,0 +1,101 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<title>BITFLIX</title>
+	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="reset.css">
+</head>
+<body>
+
+<div class="container">
+	<div class="sidebar">
+		<div class="sidebar-logo"></div>
+		<div class="sidebar-menu">
+			<div class="sidebar-item sidebar-item-active">
+				<a href="/">Главная</a>
+			</div>
+			<div class="sidebar-item">
+				<a href="/">Триллер</a>
+			</div>
+			<div class="sidebar-item">
+				<a href="/">Комедия</a>
+			</div>
+			<div class="sidebar-item">
+				<a href="/">Фантастика</a>
+			</div>
+			<div class="sidebar-item">
+				<a href="/">Избранное</a>
+			</div>
+		</div>
+	</div>
+	<div class="wrapper">
+		<div class="header">
+			<div class="header-search">
+				<div class="header-search-logo"></div>
+				<input class="header-search-input" type="text" placeholder="Поиск по каталогу...">
+				<div class="header-search-btn small-btn">ИСКАТЬ</div>
+			</div>
+			<div class="header-add-film-btn small-btn">Добавить фильм</div>
+		</div>
+		<div class="content">
+			<div class="movie-detail">
+				<div class="movie-title-wrapper">
+					<div class="movie-title">Головоломка (2015)</div>
+					<div class="movie-favorites"></div>
+				</div>
+				<div class="movie-age-wrapper">
+					<div class="movie-original-title">Inside Out</div>
+					<div class="movie-age">6+</div>
+				</div>
+				<div class="movie-detail-content">
+					<div class="movie-img"></div>
+					<div class="movie-detail-info">
+						<div class="movie-rating"></div>
+						<div class="movie-about-title detail-text-title">О фильме</div>
+						<div class="movie-about-wrapper">
+							<div class="movie-about-name-wrapper">
+								<div class="movie-about-name detail-text-s-gray">Год производства:</div>
+								<div class="movie-about-name detail-text-s-gray">Режиссер:</div>
+								<div class="movie-about-name detail-text-s-gray">В главных ролях:</div>
+							</div>
+							<div class="movie-about-desc-wrapper">
+								<div class="movie-about-desc detail-text-s-black">2012</div>
+								<div class="movie-about-desc detail-text-s-black">Пит Доктер, Роналдо Дель Кармен</div>
+								<div class="movie-about-desc detail-text-s-black">Эми Полер, Филлис Смит, Ричард Кайнд, Билл Хейдер, Льюис Блэк, Минди Кейлинг</div>
+							</div>
+
+						</div>
+						<div class="movie-desc-title detail-text-title">Описание</div>
+						<div class="movie-desc-text">Райли — обычная 11-летняя школьница, и, как у каждого из нас, ее поведение определяют пять базовых эмоций: Радость, Печаль, Страх, Гнев и Брезгливость. Эмоции живут в сознании девочки и каждый день помогают ей справляться с проблемами, руководя всеми ее поступками. До поры до времени эмоции живут дружно, но вдруг оказывается, что Райли и ее родителям предстоит переезд из небольшого уютного городка в шумный и людный мегаполис. Каждая из эмоций считает, что именно она лучше прочих знает, что нужно делать в этой непростой ситуации, и в голове у девочки наступает полная неразбериха. Чтобы наладить жизнь в большом городе, освоиться в новой школе и подружиться с одноклассниками, эмоциям Райли предстоит снова научиться работать сообща.</div>
+					</div>
+				</div>
+			</div>
+<!--			<div class="card-wrapper">-->
+<!--				<div class="card">-->
+<!--					<div class="card-img"></div>-->
+<!--					<div class="card-text-container">-->
+<!--						<div class="card-title">Не время умирать (2021)</div>-->
+<!--						<div class="card-title-translate">No Time to Die</div>-->
+<!--						<div class="card-desc">о старый друг Феликс Лейняется, когда на острове появляется его старый друг Феликс Лейтер из ЦРУ с просьбой о помощи.Джеймс Бонд оставил оперативную службу и наслаждается спокойной жизнью на Ямайке. Все меняется, когда на острове появляется его старый друг Феликс Лейтер из ЦРУ с просьбой о помощи.Джеймс Бонд оставил оперативную службу и наслаждается спокойной жизнью на Ямайке. Все меняется, когда на острове появляется его старый друг Феликс Лейтер из ЦРУ с просьбой о помощи.Джеймс Бонд оставил оперативную службу и наслаждается спокойной жизнью на Ямайке. Все меняется, когда на острове появляется его старый друг Феликс Лейтер из ЦРУ с просьбой о помощи.Джеймс Бонд оставил оперативную службу и наслаждается спокойной жизнью на Ямайке. Все меняется, когда на острове появляется его старый друг Феликс Лейтер из ЦРУ с просьбой о помощи.Джеймс Бонд оставил оперативную службу и наслаждается спокойной жизнью на Ямайке. Все меняется, когда на острове появляется его старый друг Феликс Лейтер из ЦРУ с просьбой о помощи.</div>-->
+<!--						<div class="card-length-genre-wrapper">-->
+<!--							<div class="card-length-wrapper">-->
+<!--								<div class="card-clock"></div>-->
+<!--								<div class="card-length">163 мин. / 02:43</div>-->
+<!--							</div>-->
+<!--							<div class="card-genre">боевик, триллер, приключения</div>-->
+<!--						</div>-->
+<!--					</div>-->
+<!--					<div class="card-overlay-wrapper">-->
+<!--						<div class="card-overlay"></div>-->
+<!--						<div class="card-detail-btn">Подробнее</div>-->
+<!--					</div>-->
+<!--				</div>-->
+<!--			</div>-->
+		</div>
+	</div>
+</div>
+
+</body>
+</html>
